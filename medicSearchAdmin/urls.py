2488591769ba003df2e160bb99788981d8c3817a
Paulo Peter <eduardopeter91@gmail.com>
2023-09-18 10:24:35 -0300

    Media_url/Media_root/upload img

--- medicSearchAdmin/urls.py
+++ medicSearchAdmin/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#  IMPORTAÇÕES PARA UPLOAD DE IMAGENS -------------------------
+from django.conf.urls.static import static
+from django.conf import settings
 
+#  + STATIC ADICIONADO SAO FINAL DE URLPATTERNS ------------------------
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
