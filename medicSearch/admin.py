5530b280d149ada5659bb0378268601fa0419df6
Paulo Peter <eduardopeter91@gmail.com>
2023-09-19 11:20:59 -0300

    Custom Admin/Model ProfileAdmin and Register

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import *  
+from .models import *  # IMPORTA AS MODELS PARA REGISTRO ---------
+
+# CUSTOMIZANDO A MODEL PROFILE --------------------------------
+class ProfileAdmin(admin.ModelAdmin):
+    # CRIA UM FILTRO DE HIERARQUIA COM DATAS ------------------------
+    date_hierarchy = 'created_at'
+
+
 
 # Register your models here.
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)  # Model ProfileAdmin REGISTRADA ----------------
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Neighborhood)
