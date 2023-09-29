e819b352b80ae012e4743c7af110786738380128
Paulo Peter <eduardopeter91@gmail.com>
2023-09-29 10:59:53 -0300

    added NAME attribute in urls

--- medicSearch/urls/ProfileUrls.py
+++ medicSearch/urls/ProfileUrls.py
@@ -3,6 +3,9 @@ from medicSearch.views.ProfileView import list_profile_view
 
 urlpatterns = [
     # Duas linhas chamando a mesma URL
-    path("", list_profile_view), # Chama o metodo sem passar ID (ID deve estar setado como NONE dentro do metodo para não gerar erro)
-    path("<int:id>", list_profile_view), #URL CUSTOMIZADA onde o ID recebe um valor inteiro (neste caso o ID não será NONE)
+    path("", list_profile_view, name='profiles'), # Chama o metodo sem passar ID (ID deve estar setado como NONE dentro do metodo para não gerar erro)
+    path("<int:id>", list_profile_view, name='profile'), #URL CUSTOMIZADA onde o ID recebe um valor inteiro (neste caso o ID não será NONE)
 ]
+# Foram inseridos os atributos NAME em cada url para personalização dos links
+# Caso seja necessário trocar a url de view, Não será preciso mexer em cada template da aplicação
+# pois o atributo NAME funcionará como uma VARIÀVEL 
