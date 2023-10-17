363285e7477d6f7d24a5d7366af7fc566bc91bb0
Paulo Peter <eduardopeter91@gmail.com>
2023-10-17 10:40:06 -0300

    Templates(pt.1) / Base /Home/Static

--- medicSearch/views/HomeView.py
+++ medicSearch/views/HomeView.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse # Importa a classe HttpResponse permitindo retornar a resposta para o CLIENT
+from django.shortcuts import render
 
 # Método que vamos disparar através da URL
 # REQUEST: Parametro padrão da VIEW que recupera da dos da requisição (GET ou POST)
 def home_view(request):
-    return HttpResponse('<h1>Olá mundo!</h1>')
+    return render(request, template_name='home/home.html', status=200)
--- medicSearchAdmin/urls.py
+++ medicSearchAdmin/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for medicSearchAdmin project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 #  IMPORTAÇÕES PARA UPLOAD DE IMAGENS -------------------------
 from django.conf.urls.static import static
@@ -26,5 +11,5 @@ urlpatterns = [
     # Adiciona as URLS do meu app MEDICSEARCH (medicsearch/urls/NOME DA URL) --------------------------------
     path('',include('medicSearch.urls.HomeUrls')),
     path('profile/', include('medicSearch.urls.ProfileUrls')), # PASSAR O PREFIXO QUE REFERENCIA A URL CRIADA (profile/)
-    path('medics/'), include('medicSearch.urls.MedicsUrls'), # PASSAR O PREFIXO QUE REFERENCIA A URL CRIADA (medic/)
+    path('medics/', include('medicSearch.urls.MedicUrls')), # PASSAR O PREFIXO QUE REFERENCIA A URL CRIADA (medic/)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
