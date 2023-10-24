0f04100c064662568e132653b9887e8ccf2a6093
Paulo Peter <eduardopeter91@gmail.com>
2023-10-24 11:18:37 -0300

    Create template profile

--- medicSearch/views/ProfileView.py
+++ medicSearch/views/ProfileView.py
@@ -1,10 +1,27 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from medicSearch.models import Profile
+from django.core.paginator import Paginator
 
 
 def list_profile_view(request, id=None):
-    # Modificação para retornar o Usuário que está logado usando o parâmetro REQUEST
-    if id is None and request.user.is_authenticated: # Se estiver logado (request.user.is_authenticated)
-        id = request.user.id # Retorna o ID do usuário logado
-    elif not request.user.is_authenticated: # Se NÂO estiver logado (NOT request.user.is_authenticated)
-        id = 0 # Retorna o ID com o valor zero
-    return HttpResponse('<h1>Usuário de id %s!</h1>' % id)
+    profile = None
+    if id is None and request.user.is_authenticated:
+        profile = Profile.objects.filter(user=request.user).first()
+    elif id is not None:
+        profile = Profile.objects.filter(user__id=id).first()
+    elif not request.user.is_authenticated:
+        return redirect(to='/')
+    
+    favorites = profile.show_favorites()
+    if len(favorites) > 0:
+        paginator = Paginator(favorites, 8)
+        page = request.GET.get('page')
+        favorites = paginator.get_page(page)
+
+    context = {
+        'profile': profile,
+        'favorites': favorites
+    }
+
+
+    return render(request, template_name='profile/profile.html', context=context, status=200)
