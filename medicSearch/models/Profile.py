9b95c47abb0d52792a71517c208ca5dbedab01cf
Paulo Peter <eduardopeter91@gmail.com>
2023-10-26 10:44:03 -0300

    Create and config profile - Paciente/Médico

--- medicSearch/models/Profile.py
+++ medicSearch/models/Profile.py
@@ -57,3 +57,6 @@ class Profile(models.Model):
         ids = [result.id for result in self.favorites.all()]
         return Profile.objects.filter(user__id__in=ids)
     
+    def show_ratings(self):
+        from .Rating import Rating
+        return Rating.objects.filter(user_rated=self.user)
