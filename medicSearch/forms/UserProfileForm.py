e7fb7cff5f9b73f9eadb023505658189ff73d9a9
Paulo Peter <eduardopeter91@gmail.com>
2023-11-09 10:23:53 -0300

    Forms - Create Model Forms

---
+++ medicSearch/forms/UserProfileForm.py
@@ -0,0 +1,30 @@
+from django.forms import ModelForm
+from django import forms
+from medicSearch.models.Profile import Profile
+
+
+
+class UserProfileForm(ModelForm):
+     def __init__(self, *args, **kwargs):
+          super(UserProfileForm, self).__init__(*args, **kwargs)
+          if self.instance and self.instance.role != 1:
+               del self.fields['role']
+               
+
+     class Meta:
+          model = Profile
+          fields = ['user', 'role', 'birthday', 'image']
+          #Usamos '__all__' para exibir todos os campos como itens do formulário
+          # fields ='__all__'
+          #Usamos uma lista quando queremos exibir os campos específicos
+          # fields = ['pub_date', 'headline', 'content', 'reporter']
+
+          #Usamos exclude para excluir campos específicos do sistema
+          # exclude = ['']
+          widgets = {
+               'user' : forms.HiddenInput(),
+               'role' : forms.Select(attrs={'class': "form-control"}),
+               'birthday' : forms.DateInput(attrs={'class': "form-control", "type" : "date"}),
+               'image' : forms.FileInput(attrs={'class': "form-control"})
+
+          }
